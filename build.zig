8bf2985ccdd8a8347617aed4e60cfea5110c13f6
Quentin_T <quentin.tessier@epitech.eu>
2024-11-15 09:34:25 +0100

    Using addModule to create a public module to use the repository has dep

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.createModule(.{
+    _ = b.addModule("zHDR", .{
         .root_source_file = b.path("src/hdr.zig"),
         .target = target,
         .optimize = optimize,
